e2e83ff1096dcfcfe00640888c4fa4db06a2884a
Arthur Rump <hello@arthurrump.com>
2020-06-12 16:59:01 +0200

    Add textual results

--- src/main/kotlin/nl/utwente/apollo/Apollo.kt
+++ src/main/kotlin/nl/utwente/apollo/Apollo.kt
@@ -12,6 +12,42 @@ import java.nio.file.Path
 import java.util.function.BiPredicate
 import java.util.stream.Collectors
 
+object Apollo {
+    fun verbalize(probability: Double): String {
+        return when {
+            probability <= 0.20  -> "improbable"
+            probability <= 0.375 -> "uncertain"
+            probability <= 0.675 -> "fifty-fifty"
+            probability <= 0.80  -> "expected"
+            probability <= 0.95  -> "probable"
+            else                 -> "almost certain"
+        }
+    }
+
+    fun formatResults(metrics: Map<Metrics, Double>): String {
+        val s = StringBuilder()
+        s.appendln("Based on this submission, Apollo thinks it is")
+
+        s.append("- ")
+        s.append(verbalize(DrawingReportRule.calculateFinal(metrics)))
+        s.appendln(" that the student can write a program that uses graphical commands to draw to the screen")
+
+        s.append("- ")
+        s.append(verbalize(LoopReportRule.calculateFinal(metrics)))
+        s.appendln(" that the student can write a program that uses looping constructs for repetition")
+
+        s.append("- ")
+        s.append(verbalize(OoReportRule.calculateFinal(metrics)))
+        s.appendln(" that the student can compose a program using classes, objects and methods to structure the code in an object-oriented way")
+
+        s.append("- ")
+        s.append(verbalize(MessagePassingReportRule.calculateFinal(metrics)))
+        s.appendln(" that the student can implement message passing to enable communication between classes in a complex program")
+
+        return s.toString()
+    }
+}
+
 fun main(args: Array<String>) {
     if (args.size < 1) {
         println("Usage: <project path>")
@@ -34,8 +70,6 @@ fun main(args: Array<String>) {
     }
 
     println("\nConclusions:")
-    println("Drawing: ${DrawingReportRule.calculateFinal(metrics)}")
-    println("Loops: ${LoopReportRule.calculateFinal(metrics)}")
-    println("OO: ${OoReportRule.calculateFinal(metrics)}")
-    println("Message passing: ${MessagePassingReportRule.calculateFinal(metrics)}")
+    print(Apollo.formatResults(metrics))
 }
+
